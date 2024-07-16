bbb5407ee01abae94c0918a406487d613da146e0
Artem G <itemun@gmail.com>
2024-07-16 12:45:14 +0300

    fix: generate protobuf

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ lint:
 	GOBIN=$(LOCAL_BIN) golangci-lint run ./... --config .golangci.yaml
 
 install-deps:
-	GOBIN=$(LOCAL_BIN) go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.59.1
+	GOBIN=$(LOCAL_BIN) go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28.1
 	GOBIN=$(LOCAL_BIN) go install -mod=mod google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
 
 get-deps:
--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -1,9 +1,47 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	desc "github.com/agbgru/auth/pkg/auth_v1"
 )
 
+const grpcPort = 50051
+
+type server struct {
+	desc.UnimplementedAuthServer
+}
+
+// CreateUser creates new user.
+func (s *server) CreateUser(ctx context.Context, req *desc.CreateUserRequest,
+) (*desc.CreateUserResponse, error) {
+	_ = ctx
+
+	log.Printf("CreateUser called with name: %s, email: %s, password: %s, password confirm: %s\n",
+		req.Name, req.Email, req.Password, req.PasswordConfirm)
+
+	return &desc.CreateUserResponse{Id: 1}, nil
+}
+
 func main() {
-	log.Println("Hello world")
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterAuthServer(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
