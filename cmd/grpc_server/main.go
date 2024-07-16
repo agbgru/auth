b0fe237aaee40f2ded87c65d17ca11bf40e4eeb6
Artem G <itemun@gmail.com>
2024-07-16 13:08:17 +0300

    fix: change server port

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"log"
 	"net"
 
@@ -12,7 +13,7 @@ import (
 	desc "github.com/agbgru/auth/pkg/auth_v1"
 )
 
-const grpcPort = 50051
+const grpcPort = 50052
 
 type server struct {
 	desc.UnimplementedAuthServer
@@ -23,10 +24,45 @@ func (s *server) CreateUser(ctx context.Context, req *desc.CreateUserRequest,
 ) (*desc.CreateUserResponse, error) {
 	_ = ctx
 
-	log.Printf("CreateUser called with name: %s, email: %s, password: %s, password confirm: %s\n",
-		req.Name, req.Email, req.Password, req.PasswordConfirm)
+	log.Printf("CreateUser called with name: %s, email: %s, password: %s, password confirm: %s, role: %s\n",
+		req.GetName(), req.GetEmail(), req.GetPassword(), req.GetPasswordConfirm(), req.GetRole().String())
 
 	return &desc.CreateUserResponse{Id: 1}, nil
+}
+
+// GetUser retrieves a user by ID.
+func (s *server) GetUser(ctx context.Context, req *desc.GetUserRequest) (*desc.GetUserResponse, error) {
+	_ = ctx
+
+	log.Printf("GetUser called with id: %d\n", req.GetId())
+
+	return &desc.GetUserResponse{
+		Id:        req.GetId(),
+		Name:      "John Doe",
+		Email:     "john.doe@example.com",
+		Role:      desc.Role_USER,
+		CreatedAt: timestamppb.Now(),
+		UpdatedAt: timestamppb.Now(),
+	}, nil
+}
+
+// UpdateUser updates user details.
+func (s *server) UpdateUser(ctx context.Context, req *desc.UpdateUserRequest) (*desc.UpdateUserResponse, error) {
+	_ = ctx
+
+	log.Printf("UpdateUser called with id: %d, name: %s, email: %s, role: %s\n",
+		req.GetId(), req.GetName(), req.GetEmail(), req.GetRole().String())
+
+	return &desc.UpdateUserResponse{}, nil
+}
+
+// DeleteUser deletes a user by ID.
+func (s *server) DeleteUser(ctx context.Context, req *desc.DeleteUserRequest) (*desc.DeleteUserResponse, error) {
+	_ = ctx
+
+	log.Printf("DeleteUser called with id: %d\n", req.GetId())
+
+	return &desc.DeleteUserResponse{}, nil
 }
 
 func main() {
